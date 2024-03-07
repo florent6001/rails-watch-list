d0929e63a72f67c0f723311ba4b56fcab9b5a91d
Florent Vandroy <florentvandroy@gmail.com>
2024-03-07 12:45:32 +0100

    Add bookmarks controller, routes, view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "posts#index"
-  resources :lists, only: %w[index new show create]
+  root 'lists#index'
+  resources :lists, only: %w[index new show create] do
+    resources :bookmarks, only: %w[new create]
+  end
+  resources :bookmarks, only: %w[destroy]
 end
