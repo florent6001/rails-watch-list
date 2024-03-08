14722dbf9c10d6a1d4ce352728211439e627032c
Florent Vandroy <florentvandroy@gmail.com>
2024-03-08 12:09:48 +0100

    Add photo for lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,9 @@
 class List < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks
   has_many :reviews
   has_many :movies, through: :bookmarks, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 end
