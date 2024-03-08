cb9587ef570061329d063540bdeb2d9f3bafd0cb
Florent Vandroy <florentvandroy@gmail.com>
2024-03-08 12:09:31 +0100

    Add space for form, add review validations

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,7 +6,7 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to list_path(@list)
     else
-      render template: 'lists/show', locals: { list: @list }
+      render template: 'lists/show', locals: { list: @list }, status: :unprocessable_entity
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :list
   validates :comment, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 5, only_integer: true }
 end
